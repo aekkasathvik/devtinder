f6240e06ffaeb9c0434dc0f6a2ac4aec4a2f2f7d
aekkasathvik <aekkasathvik2002@gmail.com>
2025-12-03 18:04:43 +0530

    worked in route handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const  express=require('express');
 const app= express();
-app.get('/new',(req,res)=>{
-    res.send('response from express server');
-});
-app.use((req,res)=>{res.send("useing a middleware");});
+
+app.get('/' ,(req,res,next)  => {
+    next();
+    res.send("first route handler");
+  
+  },
+  (req,res,next)=>{
+    next();
+    res.send("second route handler");},
+
+  (req,res,next)=>{
+    next();
+    res.send("Third route handler");},
+
+  (req,res,next)=>{
+    res.send("fourth route handler");}
+
+);
+app.use((req,res)=>{res.send("using a middleware");});
 app.listen(3000,()=>{console.log('server is running on port 3000');});
