1dfec088b098633cfc95ad367a5b52d8a41bd3b9
aekkasathvik <aekkasathvik2002@gmail.com>
2025-12-05 22:20:23 +0530

    added a middleware folder for removing out the authentication part

--- src/app.js
+++ src/app.js
@@ -1,22 +1,12 @@
 const  express=require('express');
 const app= express();
-
-app.get('/' ,(req,res,next)  => {
-    next();
-    res.send("first route handler");
-  
-  },
-  (req,res,next)=>{
-    next();
-    res.send("second route handler");},
-
-  (req,res,next)=>{
-    next();
-    res.send("Third route handler");},
-
-  (req,res,next)=>{
-    res.send("fourth route handler");}
-
-);
-app.use((req,res)=>{res.send("using a middleware");});
+const {adminAuth}=require('./middlewares/adminAuth');
+app.use('/admin',adminAuth);
+app.get('/admin/allUsers',(req,res)=>{
+    res.send("List of all users");
+});
+app.delete('/admin/deleteUser/:id',(req,res)=>{
+    const userId=req.params.id;
+    res.send(`User with id ${userId} deleted`);
+});
 app.listen(3000,()=>{console.log('server is running on port 3000');});
