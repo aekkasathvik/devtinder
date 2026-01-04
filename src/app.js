4ae46190de51ac5fce8666d9a6440159e1041428
aekkasathvik <aekkasathvik2002@gmail.com>
2026-01-04 19:21:45 +0530

    we have implemented the logic of hashing the password and storing in DB

--- src/app.js
+++ src/app.js
@@ -1,25 +1,75 @@
 const express = require('express');
 const app = express();
+const validator = require('validator');
 const { isValidSignUp } = require('./utils/validate');
 const User = require('./models/users');  // FIXED: Capitalized Model import
 const { connectDB } = require('./config/database');
-const {bcrypt} = require('bcrypt');
+const bcrypt= require('bcrypt');
 // Parse JSON body and convert it into JS object so that we can use the body inside route handlers 
 app.use(express.json());
 
 // Signup route
 app.post('/signup', async (req, res) => {
    try {
-       
-
+    isValidSignUp(req); // Validate the signup data
+    const {password}=req.body;
+    const hashedPassword= await bcrypt.hash(password,10);
+    req.body.password=hashedPassword;
+    const user = new User(req.body);
+    await user.save(); // Save the user to the database
+    res.status(201).send("User signed up successfully");
    }
    catch (err) {
-
+    //catch the error and send the error message to the client
+    res.status(400).send("Error during signup: " + err.message);
    }
    finally {
     console.log("Signup attempt completed");
    }
 });
+
+// login route 
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // 1. Basic validation
+        if (!email || !password) {
+            return res.status(400).send("Email and password are required");
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.status(400).send("Invalid email format");
+        }
+
+        // 2. Check user existence
+        const userRecord = await User.findOne({ email });
+
+        if (!userRecord) {
+            return res.status(400).send("Invalid credentials");
+        }
+
+        // 3. Password verification
+        const isMatch = await bcrypt.compare(
+            password,
+            userRecord.password
+        );
+
+        if (!isMatch) {
+            return res.status(400).send("Invalid credentials");
+        }
+
+        // 4. Success
+      res.cookie("abcde","hxhdid");
+        return res.status(200).send("Login successful");
+
+    } catch (err) {
+        return res.status(500).send("Bad request: " + err.message);
+    } finally {
+        console.log("login attempt completed");
+    }
+});
+
 //get the user based on his email 
 app.get('/user',async (req,res)=>{
    try{
