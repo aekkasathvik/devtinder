865c855317ded0e51205a03497221d6968538622
aekkasathvik <aekkasathvik2002@gmail.com>
2026-01-08 22:34:05 +0530

    created a routes file where we are handling endpoints of each type of route seprately and using them in app.js

---
+++ APIList.md
@@ -0,0 +1,7 @@
+All the API's related to devtinder
+-POST/signup
+-POST/login
+-POST/logout 
+-GET /profileView
+-PATCH /update/details
+-PATCH /update/password
---
+++ src/routes/authRouter.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const authRouter = express.Router();
+const User = require('../models/users');
+const bcrypt= require('bcrypt');
+const validator = require('validator');
+const { isValidSignUp } = require('../utils/validate');
+authRouter.post('/signup', async (req, res) => {
+   try {
+    isValidSignUp(req); // Validate the signup data
+    const {password}=req.body;
+    const hashedPassword= await bcrypt.hash(password,10);
+    req.body.password=hashedPassword;
+    const user = new User(req.body);
+    await user.save(); // Save the user to the database
+    res.status(201).send("User signed up successfully");
+   }
+   catch (err) {
+    //catch the error and send the error message to the client
+    res.status(400).send("Error during signup: " + err.message);
+   }
+   finally {
+    console.log("Signup attempt completed");
+   }
+});
+
+// login route 
+authRouter.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // 1. Basic validation
+        if (!email || !password) {
+            return res.status(400).send("Email and password are required");
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.status(400).send("Invalid email format");
+        }
+
+        // 2. Check user existence
+        const userRecord = await User.findOne({ email });
+
+        if (!userRecord) {
+            return res.status(400).send("Invalid credentials");
+        }
+
+        // 3. Password verification
+        const isMatch = await userRecord.validatePassword(password);
+        if (!isMatch) {
+            return res.status(400).send("Invalid credentials");
+        }
+        //we have the user now create a cookie for the user 
+        const token=await userRecord.getJWT();
+        console.log(token);
+        res.cookie("token",token);
+        return res.status(200).send("Login successful");
+
+    } catch (err) {
+        return res.status(500).send("Bad request: " + err.message);
+    } finally {
+        console.log("login attempt completed");
+    }
+});
+module.exports={authRouter};
