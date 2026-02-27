46e2d9a0ddfeecd41ac9d76b61fbb87334fff3bd
aekkasathvik <aekkasathvik2002@gmail.com>
2026-02-27 09:48:10 +0530

    feed API with pagination logic

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const userRouter=express.Router();
 const userAuth=require('../middlewares/userAuth');
+const User=require('../models/users');
 const connectionRequest=require('../models/connectionRequest');
 //get all the connection requests with status interested and allow user to accept or reject
 userRouter.get('/getAllRequest',userAuth ,async (req,res)=>{
@@ -45,4 +46,47 @@ userRouter.get('/getAllConnections',userAuth , async (req,res)=>{
         console.log('get all connection routes executed');
     }
 })
+
+//Feed API 
+userRouter.get('/feed' ,userAuth , async (req,res)=>{
+    try {
+        //get the users based on below conditions 
+        //1 we should not get the logged in user details
+        //2 we should not get the users who are connected to the logged in user
+        //3 we should not get the users who have sent connection request to the logged in user with status interested
+        const loggedInUser=req.user;
+        console.log(loggedInUser);
+        const connections =await connectionRequest.find({$or:[{fromUserId:loggedInUser._id },{toUserId:loggedInUser._id}]});
+        //loop through all the connections and store the user ids in a set
+        const hideUsers=new Set();
+        connections.forEach(connection=>{
+           if(connection.fromUserId.toString() === loggedInUser._id.toString()) {
+               hideUsers.add(connection.toUserId.toString());
+           }
+           else {
+               hideUsers.add(connection.fromUserId.toString());
+           }
+        })
+        //add the logged in User id to the set so that we do not ge the logged in user details in to the feed 
+        hideUsers.add(loggedInUser._id.toString());
+        //we got all the users we do not want in the feed now we will get all the users and filter out the users we do not want in the feed
+        console.log(hideUsers);
+        //pagination cooncept where we get the page number and limit from the query params and skip the records based on the page number and limit
+        const page=parseInt(req.params.page || 1);
+        const limit=parseInt(req.params.limit || 10);
+        const skip=(page-1)*limit;
+        const data =await User.find({_id:{$nin:Array.from(hideUsers)}}).skip(skip).limit(limit);
+        res.json({
+            message:"Feed for user "+loggedInUser.firstName,
+            data:data
+        });
+        console.log(data);
+    }
+    catch(err) {
+        res.status(400).json({"error":err.message});
+    }
+    finally {
+        console.log('Feed API executed');
+    }
+});
 module.exports=userRouter;
